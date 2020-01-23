90bb77092851a7e68d5b743f9ada3e9d2d63cc15
Woodpeaker <49099503+Woodpeaker@users.noreply.github.com>
2020-01-22 23:26:41 -0500

    Initial commit

---
+++ CyberPunkPrototype/Assets/Script/BanditAllyCon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditAllyCon : MonoBehaviour
+{
+    public Transform energyBallObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("2"))
+        {
+            GetComponent<Transform>().position = new Vector2(2.13f, -1.24f);
+            StartCoroutine(energyBallLaunch());
+            GetComponent<Animator>().SetTrigger("BanditAllyRange");
+            StartCoroutine(IniPosition());
+        }
+    }
+    IEnumerator energyBallLaunch()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(energyBallObj, new Vector2(1.5f, -0.26f), energyBallObj.rotation);
+    }
+    IEnumerator IniPosition()
+    {
+        yield return new WaitForSeconds(2);
+        GetComponent<Transform>().position = new Vector2(5.66f, -2.39f);
+    }
+}
---
+++ CyberPunkPrototype/Assets/Script/BanditCon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditCon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("1"))
+        {
+
+            GetComponent<Transform>().position = new Vector2(-3.33f,-1.6f);
+            GetComponent<Animator>().SetTrigger("Bandit_Melee");
+        }
+        
+
+    }
+
+    void IniPosition()
+    {
+        GetComponent<Transform>().position = new Vector2(4.4f, -0.97f);
+    }
+}
