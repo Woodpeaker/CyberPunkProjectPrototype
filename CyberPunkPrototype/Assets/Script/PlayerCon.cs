ce271d124e0cf6c12508e9943efc8b8e4f496312
Woodpeaker <49099503+Woodpeaker@users.noreply.github.com>
2020-03-18 04:14:59 -0400

    #2
    Added running animation to player.

--- CyberPunkPrototype/Assets/Script/PlayerCon.cs
+++ CyberPunkPrototype/Assets/Script/PlayerCon.cs
@@ -18,10 +18,18 @@ public class PlayerCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(hAxis,vAxis, 0) * speed * Time.deltaTime;
         rig.MovePosition(transform.position + movement);
+        if (hAxis !=0 || vAxis !=0 )
+        {
+            GetComponent<Animator>().SetTrigger("Player_Running");
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Player_Stand");
+        }
     }
 }
